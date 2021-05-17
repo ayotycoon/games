17922f11730cf1bb0d41106138657fb5f6338929
ayotycoon <kaysehventeen@gmail.com>
2021-05-18 00:50:57 +0100

    c

--- src/chess/engine/ChessKnight.js
+++ src/chess/engine/ChessKnight.js
@@ -11,43 +11,63 @@ export class ChessKnight extends ChessPiece {
 
     availableMoves = () => {
         let indexes = [];
-        const incY1 = !this.isPieceWhite ? 1 : -1;
-        const incY2 = !this.isPieceWhite ? 2 : -2;
+
+        // check up
 
 
 
 
-        // check if it can move a step in y axes and if its own piece isnt on its moves
-        const possiblePiece = this.board[this.positionYIndex + incY1][this.positionXIndex];
+        const analyzeAndBreak = (incY,incX) => {
+            console.log(incY,incX)
+            if(incY < 0 || incY > 7 || incX < 0 || incX >7) return true;
+           
+          
+           
+            const possiblePiece = this.board[incY][incX]
+            if (!possiblePiece) {
+                indexes.push({ positionYIndex: incY, positionXIndex: incX })
 
-        if (this.positionYIndex + incY1 < this.availableXMovements.length && this.positionYIndex + incY1 >= 0 && !possiblePiece) {
-            indexes.push({ positionYIndex: this.positionYIndex + incY1, positionXIndex: this.positionXIndex })
+            } else if (possiblePiece.isPieceWhite == this.isPieceWhite) {
+                // if its same color
+             //   return true
 
-            if (this.successfulMovements == 0) {
-                // check if its first time, only move if theres nothing in the way
-                const possiblePiece2 = this.positionYIndex + incY2 < 8 ? this.board[this.positionYIndex + incY2][this.positionXIndex] : null;
-                if (this.positionYIndex + incY2 < this.availableYMovements.length && this.positionYIndex + incY2 >= 0 && !possiblePiece2) {
-                    indexes.push({ positionYIndex: this.positionYIndex + incY2, positionXIndex: this.positionXIndex })
+            } else {
+                indexes.push({ positionYIndex: incY, positionXIndex: incX })
 
-                }
+                return true
+
             }
-
-
-
         }
 
 
 
-        const opp = this.board[this.positionYIndex + incY1][this.positionXIndex + 1]
-        if (this.positionYIndex + incY1 < this.availableYMovements.length && this.positionYIndex + incY1 >= 0 && opp && opp.isPieceWhite == !this.isPieceWhite) {
-            indexes.push({ positionYIndex: this.positionYIndex + incY1, positionXIndex: this.positionXIndex + 1 })
-            // check diagonal if an opp is there
-        }
-        const opp2 = this.board[this.positionYIndex + incY1][this.positionXIndex - 1]
-        if (this.positionYIndex + incY1 < this.availableYMovements.length && this.positionYIndex + incY1 >= 0 && opp2 && opp2.isPieceWhite == !this.isPieceWhite) {
-            indexes.push({ positionYIndex: this.positionYIndex + incY1, positionXIndex: this.positionXIndex - 1 })
-            // check diagonal if an opp is there
-        }
+       
+        const movements = [
+            [2,1],
+            [2,-1],
+            [-2,1],
+            [-2,-1],
+            [1,2],
+            [1,-2],
+            [-1,2],
+            [-1,-2],
+      
+
+
+
+        ]
+        
+
+
+            for (let i = 0; i < movements.length; i++) {
+                const curr = movements[i]
+                const incY = this.positionYIndex + curr[0];
+                const incX = this.positionXIndex + curr[1];
+               
+
+                analyzeAndBreak( incY,incX)
+            }
+          
 
 
 
@@ -65,6 +85,7 @@ export class ChessKnight extends ChessPiece {
             }
         })
     }
+
 
 
 }
